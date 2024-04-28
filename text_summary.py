bfb0cdc269de2994c4b052a1dd0d4418cc3ba346
Siya <siyadesai31@gmail.com>
2024-04-28 17:49:57 +0530

    freq of words,sentences

--- text_summary.py
+++ text_summary.py
@@ -14,3 +14,42 @@ doc = nlp(text)
 # print(doc)
 tokens = [token.text for token in doc ]
 # print(tokens)
+
+word_freq={}
+for word in doc:
+    if word.text.lower() not in stopwords and word.text.lower() not in punctuation:
+        if word.text not in word_freq.keys():
+            word_freq[word.text]=1
+        else:
+            word_freq[word.text]+=1
+
+# print(word_freq)
+
+max_freq = max(word_freq.values())
+print(max_freq)
+
+for word in word_freq.keys():
+    word_freq[word] = word_freq[word]/max_freq
+# normalised freq 
+
+# print(word_freq)
+
+sent_tokens = [sent for sent in doc.sents]
+# print(sent_tokens)
+
+sent_score = {}
+for sent in sent_tokens:
+    for word in sent:
+        if word.text in word_freq.keys():
+            if sent not in sent_score.keys():
+                sent_score[sent] = word_freq[word.text]
+            else:
+                sent_score[sent] += word_freq[word.text]
+
+# print(sent_score)
+
+select_len = int(len(sent_tokens) * 0.3)
+print(select_len)
+
+summary = nlargest(select_len,sent_score,key = sent_score.get)
+print(summary)
